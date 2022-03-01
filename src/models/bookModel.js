cfc4089dd93958fa4fbe9cd215c8333194041a86
sonu kumar rai <digitalsonu17@gmail.com>
2022-03-02 00:25:24 +0530

    my assignment

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -14,9 +14,9 @@ const bookSchema = new mongoose.Schema( {
     
     // " best boook on earth"   [ "Nodejs in detail" , "mongodb in detail", "fronend in detail"] 
     // {
-        // "ch1 ": "awesome intro to JS",
-        // "ch2" : "intro to nodejs",
-        // "ch3" : "intro to db"
+    //     "ch1 ": "awesome intro to JS",
+    //     "ch2" : "intro to nodejs",
+    //     "ch3" : "intro to db"
     //  }
     summary :  mongoose.Schema.Types.Mixed,
     isDeleted: Boolean //true on book deletion i.e you flag the document/data as isDeleted: true..(mark "dirty")
